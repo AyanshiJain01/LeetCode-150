76dbc3d0089963a31570b184e9e74151172bb816
Ayanshi Jain <ayanshijain7@gmail.com>
2026-03-17 13:48:30 +0530

    Time: 7 ms (47.71%) | Memory: 20.1 MB (35.18%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    bool chkLoop(vector<vector<int>> &graph, int i, vector<int> &visited)
+
+    bool chkLoop(vector<vector<int>> &graph, int i, vector<int> & visited)
     {
-        if(visited[i]==1)
+        if(visited[i] == 1)
         {
             return true;
         }
 
-        if(visited[i]==2)
+        if(visited[i] == 2)
         {
             return false;
         }
 
         visited[i] = 2;
 
-        //check neighbours
-        for(int index:graph[i])
+        for(int index : graph[i])
         {
             if(!chkLoop(graph,index,visited))
             {
                 return false;
             }
         }
-
         visited[i] = 1;
         return true;
     }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> graph(numCourses);
 
-        // check prerequisite and create graph
-        for(auto prerequisite : prerequisites)
+        for(auto  prerequisite : prerequisites)
         {
             int firstCourse = prerequisite[0];
             int secondCourse = prerequisite[1];
@@ -40,14 +37,13 @@ public:
         }
 
         vector<int> visited(numCourses,0);
-
         for(int i = 0; i < numCourses; i++)
         {
-            if(visited[i]!=1)
+            if(visited[i] !=1)
             {
-                bool tmp = chkLoop(graph,i,visited);
+                bool temp = chkLoop(graph,i,visited);
 
-                if(!tmp)
+                if(!temp)
                 {
                     return false;
                 }

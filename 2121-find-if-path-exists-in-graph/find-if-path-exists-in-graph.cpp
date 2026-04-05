20cd208ac2ad37adff0ad03207c5fd97455c93f9
Ayanshi Jain <ayanshijain7@gmail.com>
2026-04-05 08:33:49 +0530

    Time: 662 ms (9.75%) | Memory: 379 MB (6.52%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,14 +1,15 @@
 class Solution {
-    bool dfs(unordered_map<int, vector<int>>& graph, int src,int dst, vector<bool> &visited, int n)
+public:
+
+    bool dfs(unordered_map<int,vector<int>>&graph, int src, int dest, vector<bool> &vis, int n)
     {
-        if(src == dst) return true;
-        visited[src] = true;
+        if(src == dest) return true;
+        vis[src] = true;
 
         for(auto neighbor : graph[src])
         {
-            if(!visited[neighbor])
-            {
-                if(dfs(graph,neighbor,dst,visited,n))
+            if(!vis[neighbor]){
+                if(dfs(graph,neighbor,dest,vis,n))
                 {
                     return true;
                 }
@@ -16,18 +17,17 @@ class Solution {
         }
         return false;
     }
-public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int, vector<int>> graph;
+        unordered_map<int,vector<int>>graph;
         for(auto edge : edges)
         {
             int u = edge[0];
             int v = edge[1];
+
             graph[u].push_back(v);
             graph[v].push_back(u);
         }
-
-        vector<bool> visited(n,false);
-      return  dfs(graph,source,destination,visited,n);
+        vector<bool>vis(n,false);
+        return dfs(graph,source,destination,vis,n);
     }
 };
